9d4e8c3ccd433d55feecacac226e74b73db26931
singularitti <singularitti@outlook.com>
2020-12-14 23:33:47 -0500

    Add command `print` & `install`

--- src/XPS.jl
+++ src/XPS.jl
@@ -1,5 +1,34 @@
 module XPS
 
-# Write your package code here.
+using AbInitioSoftwareBase: load, extension
+using Comonicon: @cast, @main
+using Pkg: @pkg_str
+using Serialization: deserialize
+
+@cast function print(file)
+    ext = lowercase(extension(file))
+    data = if ext == "jls"
+        deserialize(file)
+    elseif ext in ("json", "yaml", "yml", "toml")
+        load(file)
+    else
+        error("unsupported file extension `$ext`!")
+    end
+    display(data)
+end
+
+@cast function install(plugin)
+    name = lowercase(plugin)
+    if name == "qe"
+        pkg"add https://github.com/MineralsCloud/QuantumESPRESSOExpress.jl.git"
+    else
+        error("unsupported plugin `$name`!")
+    end
+end
+
+"""
+my demo Comonicon CLI project.
+"""
+@main
 
 end
