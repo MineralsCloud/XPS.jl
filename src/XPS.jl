b158ae0bbce2b71b3317fb5d0dfb2539dd0f39f5
Reno <25192197+singularitti@users.noreply.github.com>
2021-09-06 19:28:24 -0400

    Add `load_plugin`

--- src/XPS.jl
+++ src/XPS.jl
@@ -4,7 +4,7 @@ using AbInitioSoftwareBase: load, extension
 using Comonicon: @cast, @main
 using EquationsOfStateOfSolids: Parameters, EquationOfStateOfSolids
 using Pkg: add, rm, gc
-using Preferences: @set_preferences!
+using Preferences: @set_preferences!, @load_preference
 using PrettyPrint: pprint
 using Serialization: deserialize
 
@@ -54,6 +54,15 @@ end
     name = lowercase(plugin)
     if name == "qe"
         @set_preferences!("plugin" => "Quantum ESPRESSO")
+    else
+        error("unsupported plugin `$name`!")
+    end
+end
+
+function load_plugin()
+    name = @load_preference("plugin")
+    if name == "qe"
+        eval(:(using QuantumESPRESSOExpress))
     else
         error("unsupported plugin `$name`!")
     end
